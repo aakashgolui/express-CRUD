a57ec2233ff5d1069d25edd00285234627f930a0
aakashgolui <iamakashgolui140@gmail.com>
2025-06-09 18:26:12 +0530

    more changes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import path,{ dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+//setup static folder
+// app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}"`);
